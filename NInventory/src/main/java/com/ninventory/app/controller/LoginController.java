e9d7969d75cc2830f68e1aa4746ca5cb484ed43f
Muhammad Masud Rana <info@inlbd.com>
2020-03-11 00:27:08 +0600

    working on login not completed

--- NInventory/src/main/java/com/ninventory/app/controller/LoginController.java
+++ NInventory/src/main/java/com/ninventory/app/controller/LoginController.java
@@ -1,12 +1,31 @@
 package com.ninventory.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ninventory.app.model.Users;
 
 @Controller
 public class LoginController {
 	  @GetMapping(value = "/")
-	  public String available() {
+	  public String available(Model model) {
+		model.addAttribute("user1", new Users()); 
 	    return "login/login";
 	  }
+	  
+	  @PostMapping(value ="/login")
+	  public void Login( Users user ) {
+		System.out.println(user.getName());  
+		  
+		  
+	  }
+	  
+	  
+	  
+	  
+	  
+	  
 }
